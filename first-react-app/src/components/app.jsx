e07fb39f6889f238ac779533585e3f822b3582f1
Asionsolver <asionsolver274@gmail.com>
2022-11-27 00:36:30 +0600

    React State Example by Implementing A Timer

--- first-react-app/src/components/app.jsx
+++ first-react-app/src/components/app.jsx
@@ -1,31 +1,66 @@
 import React,{Component} from "react";
+import './app.style.css';
 
 class App extends Component {
-
-    // constructor(props) {
-    //     super(props);
-    //     this.count = 0;
-    //     this.state = {
-    //         count: 0
-    //     };
-    // }
-
     state = {
         count: 0
     };
-    // count = 10;
+
+    intervalId = null;
+
+    incrementCount = () => {
+        this.setState({count: this.state.count + 1});
+    };
+
+    decrementCount = () => {
+        if (this.state.count > 0) {
+            this.setState({count: this.state.count - 1});
+        } 
+    };
+
+    startTimer = () => {
+        if (this.state.count > 0 && !this.intervalId) {
+            this.intervalId = setInterval(() => {
+                this.setState({count: this.state.count - 1},()=>{
+                    if (this.state.count === 0) {
+                        alert("Time is up!");
+                        clearInterval(this.intervalId);
+                        this.intervalId = null;
+                    }
+                });
+            }, 1000 );
+        }
+    }
+
+    stopTimer = () => {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+
+    resetTimer = () => {
+        this.setState({count: 0});
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
 
     render() {
-        // console.log(this.count);
         return (
-            <div className="App  ">
-                <h1>Why do We Need State in React</h1>
-                <h1>Count = {this.state.count}</h1>
-                <button onClick={()=> {
-                        // this.count++;
-                        this.setState({count: this.state.count + 1});
-                        console.log('Clicked...',this.state.count);
-                }}> Increment</button>
+            <div className="app">
+                <h1 className="heading">Simple Timer</h1>
+                <div className="container">
+                    <button className="btn" onClick={this.decrementCount}> - </button>
+                    <span className="text">{this.state.count}</span>
+                    <button className="btn" onClick={this.incrementCount}> + </button>
+                </div>
+
+                <div className="container">
+                    <button onClick={this.startTimer} className="btn">Start</button>
+                    <button onClick={this.stopTimer} className="btn">Stop</button>
+                    <button onClick={this.resetTimer} className="btn">Reset</button>
+                </div>
             </div>
         );
     }
---
+++ first-react-app/src/components/app_update_state.jsx
@@ -0,0 +1,43 @@
+import React,{Component} from "react";
+
+class App extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.count = 0;
+    //     this.state = {
+    //         count: 0
+    //     };
+    // }
+
+    state = {
+        count: 0
+    };
+    // count = 10;
+
+    render() {
+        // console.log(this.count);
+        return (
+            <div className="App  ">
+                <h1>Why do We Need State in React</h1>
+                <h1>Count = {this.state.count}</h1>
+                <button onClick={()=> {
+                        // this.count++;
+                        // this.setState({count: this.state.count + 1});
+                        this.setState((prev)=>{
+                            return{
+                                count: prev.count + 1
+                            }
+                        },()=>
+                        {
+                        console.log('Clicked...',this.state.count);
+                        });
+                        // this.state.count = this.state.count + 1; 
+                        
+                }}> Increment</button>
+            </div>
+        );
+    }
+}
+
+export default App;
