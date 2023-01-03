c698fc12668156333b85beb9fd7385e0ef903306
Asionsolver <asionsolver274@gmail.com>
2023-01-03 23:51:03 +0600

    Learn new topic like functional component

---
+++ react-practic/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import FunctionalComponents from './functional_components/FunctionalComponents';
+
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalComponents />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-practic/src/functional_components/FunctionalComponents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FunctionalComponents() {
+  return (
+    <div>
+      <h1>Functional Components</h1>
+    </div>
+  );
+}
+
+export default FunctionalComponents;
