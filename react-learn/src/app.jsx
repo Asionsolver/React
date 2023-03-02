9f8399f2f4c77c5e5536ff9cd5afaf00500cc967
Asionsolver <asionsolver274@gmail.com>
2023-03-02 20:36:45 +0600

    add new project like Tic-Tac-Toe

--- react-learn/src/app.jsx
+++ react-learn/src/app.jsx
@@ -1,37 +1,18 @@
-import React, { Component } from 'react';
-import './components/signup-form/app.module.css';
-import SignUp from './components/signup-form/index';
+import React from 'react';
+import './components/ticTacToe/app.css';
+import Game from './components/ticTacToe/game';
 
-class App extends Component {
-  state = {
-    users: []
-  };
 
-  createUser = user => {
-    user.id = new Date().getTime().toString();
-    this.setState({
-      users: [...this.state.users, user]
-    });
-  };
-  render() {
+const App = () => {
     return (
-      <div>
-        <SignUp createUser={this.createUser}/>
-        <div className="my-5">
-          <h3>All Registered Users</h3>
-          <ul className="list-group">
-            {this.state.users.map(user => (
-              <li key={user.id} className="list-group-item">
-                {user.name}->{user.email}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className='app'>
+        <h1>React Tic Tac Toe</h1>
+        <Game/>
       </div>
 
     )
   }
-}
+
 
 
 export default App;
